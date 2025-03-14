75cd8911ab7eacbdfef5d9d0e23a2bf58037eef1
Hafeezbaig <hafeezbaig3138@gmail.com>
2025-03-14 11:18:53 +0530

    select image component updated and bugs fixed

--- app/(routes)/dashboard/_components/ImageUpload.tsx
+++ app/(routes)/dashboard/_components/ImageUpload.tsx
@@ -1,22 +1,52 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
-import { CloudUploadIcon } from 'lucide-react'
-import React from 'react'
+import { CloudUploadIcon, X } from 'lucide-react'
+import Image from 'next/image'
+import React, { ChangeEvent, useState } from 'react'
 
 function ImageUpload() {
+
+    const [previewUrl, setPreviewUrl]=useState<string|null>(null)
+
+    const OnImageSelect=(event:ChangeEvent<HTMLInputElement>)=>{
+        const files=event.target.files;
+        if(files)
+        {
+            console.log(files[0])
+            const imageUrl=URL.createObjectURL(files[0])
+            setPreviewUrl(imageUrl)
+        }
+    }
   return (
     <div className='mt-10'>
         <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
-            <div className='p-7 border border-dashed rounded-md shadow-md flex flex-col items-center justify-center'>
+            {!previewUrl? <div className='p-7 border border-dashed rounded-md shadow-md flex flex-col items-center justify-center'>
                 <CloudUploadIcon className='h-10 w-10' />
                 <h2 className='font-bold text-lg'>Upload Image</h2>
 
                 <p className='text-gray-400 mt-3'>Select Wireframe Image</p>
                 <div className='p-5 border border-dashed w-full flex mt-7 justify-center'>
-                    <Button>
-                        Select Image
-                    </Button>
+                    <label htmlFor='imageSelect'>
+                        <h2 className='p-2 bg-primary text-white rounded-md px-3 cursor-pointer'>
+                            Select Image
+                        </h2>
+                    </label>
+                    
                 </div>
+                <input type="file" id='imageSelect' className='hidden' 
+                multiple={false}
+                onChange={OnImageSelect} />
+            </div>:
+            <div className='p-5 border border-dashed'>
+                <Image src={previewUrl} alt='preview' width={500} height={500} 
+                className='w-full h-[300px] object-contain' />
+                
+                <X className='flex justify-end w-full cursor-pointer' 
+                onClick={()=>setPreviewUrl(null)} />
+
             </div>
+}
             <div>
                 User Input Text Area
             </div>
