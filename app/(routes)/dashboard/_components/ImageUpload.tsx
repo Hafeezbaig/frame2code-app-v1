914a6e754e0e75f3c1648da06c723a3f95c920fb
Hafeezbaig <hafeezbaig3138@gmail.com>
2025-03-15 15:42:12 +0530

    firebase file storage connected

--- app/(routes)/dashboard/_components/ImageUpload.tsx
+++ app/(routes)/dashboard/_components/ImageUpload.tsx
@@ -6,6 +6,8 @@ import { CloudUploadIcon, WandSparkles, X } from 'lucide-react'
 import Image from 'next/image'
 import React, { ChangeEvent, useState } from 'react'
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
+import { storage } from '@/configs/firebaseConfig'
 
 function ImageUpload() {
 
@@ -26,25 +28,47 @@ function ImageUpload() {
 
     const [previewUrl, setPreviewUrl] = useState<string | null>(null)
 
+    const [file, setFile] = useState<any>();
+
+    const [model, setModel] = useState<string>();
+    const [description, setDescription] = useState<string>();
     const OnImageSelect = (event: ChangeEvent<HTMLInputElement>) => {
         const files = event.target.files;
         if (files) {
             console.log(files[0])
             const imageUrl = URL.createObjectURL(files[0])
+            setFile(files[0]);
             setPreviewUrl(imageUrl)
         }
     }
+
+    const OnConvertToCodeButtonClick=async()=>{
+        if(!file||!model||!description){
+            console.log('Please fill all the fields');
+            return;
+        }
+        // save image to firebase storage
+
+        const fileName=Date.now()+'.png';
+        const imageRef=ref(storage,"Wireframe_To_Code_Project/"+fileName);
+        await uploadBytes(imageRef,file).then(resp=>{
+            console.log('Image Uploaded Successfully')
+        });
+        const imageUrl=await getDownloadURL(imageRef);
+        console.log(imageUrl);
+    }
+
     return (
         <div className='mt-10'>
             <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
                 {!previewUrl ? <div className='p-7 border border-dashed rounded-md shadow-md flex flex-col items-center justify-center'>
                     <CloudUploadIcon className='h-10 w-10' />
-                    <h2 className='font-bold text-lg'>Upload Image</h2>
+                    <h2 className='font-bold text-lg '>Upload Image</h2>
 
                     <p className='text-gray-400 mt-3'>Select Wireframe Image</p>
                     <div className='p-5 border border-dashed w-full flex mt-7 justify-center'>
                         <label htmlFor='imageSelect'>
-                            <h2 className='p-2 bg-primary text-white rounded-md px-3 cursor-pointer'>
+                            <h2 className='p-2 text-white rounded-md px-3 cursor-pointer bg-blue-600 hover:bg-blue-700'>
                                 Select Image
                             </h2>
                         </label>
@@ -66,7 +90,7 @@ function ImageUpload() {
                 <div className='p-7 border shadow-md rounded-lg'>
 
                     <h2 className='font-bold text-lg'>Select AI Model</h2>
-                    <Select>
+                    <Select onValueChange={(value)=>setModel(value)}>
   <SelectTrigger className="w-full">
     <SelectValue placeholder="Select AI Model" />
   </SelectTrigger>
@@ -86,11 +110,13 @@ function ImageUpload() {
 
 
                     <h2 className='font-bold text-lg mt-7'>Enter Description about your web page</h2>
-                    <Textarea className='mt-3 h-[200px]' placeholder='Write about your web page' />
+                    <Textarea 
+                    onChange={(event)=>setDescription(event?.target.value)}
+                    className='mt-3 h-[200px]' placeholder='Write about your web page' />
                 </div>
             </div>
-            <div className='mt-10 flex items-center justify-center font-bold'>
-                <Button> <WandSparkles /> Convert to Code</Button>
+            <div className='mt-10 flex items-center justify-center font-bold '>
+                <Button className='bg-blue-600 hover:bg-blue-700 text-white' onClick={OnConvertToCodeButtonClick}> <WandSparkles /> Convert to Code</Button>
             </div>
         </div>
     )
