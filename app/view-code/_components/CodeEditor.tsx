06d1251e8d4a09785d3b6ba8a2de84bf143194a8
Hafeezbaig <hafeezbaig3138@gmail.com>
2025-03-19 15:08:24 +0530

    sandpack code editor sdk integrated

--- app/view-code/_components/CodeEditor.tsx
+++ app/view-code/_components/CodeEditor.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Sandpack } from '@codesandbox/sandpack-react'
 
 function CodeEditor() {
   return (
-    <div>CodeEditor</div>
+    <div>
+        <Sandpack template='react'
+        files={{
+            "/App.js": `export default function App() {
+            return <h1>Hello, World!</h1>
+        }`
+        }}
+        />
+    </div>
   )
 }
 
